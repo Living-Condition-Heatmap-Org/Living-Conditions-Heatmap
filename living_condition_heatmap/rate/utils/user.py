898dfd82765397579d9df68455d12f6e7707c2cb
IsseiMori <ganntyu1225issei@gmail.com>
2023-03-12 18:07:26 -0700

    fix: extracting token

--- living_condition_heatmap/rate/utils/user.py
+++ living_condition_heatmap/rate/utils/user.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 def get_user(request):
-    token = request.META.get(settings.AUTHORIZATION_HEADER).split(" ")[1]
+    token = request.META.get(settings.AUTHORIZATION_HEADER).split(" ")[2]
     url = settings.AUTHENTICATION_URL + token
     r = requests.get(url)
     user_key=r.json()["email"]
